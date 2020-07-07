8d92d035ce06f7e65cd98ab1d3e8ec22a3801ef8
JungHyeokChoi <page1114@gmail.com>
2020-07-07 15:29:28 +0900

    Server setting

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -60,30 +60,4 @@ JavaScript:
   You can run the query application as follows. By default, the query application will
   return all cars, but you can update the application to evaluate other transactions:
     node query
-
-TypeScript:
-
-  Start by changing into the "typescript" directory:
-    cd typescript
-
-  Next, install all required packages:
-    npm install
-
-  Next, compile the TypeScript code into JavaScript:
-    npm run build
-
-  Then run the following applications to enroll the admin user, and register a new user
-  called user1 which will be used by the other applications to interact with the deployed
-  FabCar contract:
-    node dist/enrollAdmin
-    node dist/registerUser
-
-  You can run the invoke application as follows. By default, the invoke application will
-  create a new car, but you can update the application to submit other transactions:
-    node dist/invoke
-
-  You can run the query application as follows. By default, the query application will
-  return all cars, but you can update the application to evaluate other transactions:
-    node dist/query
-
 EOF
