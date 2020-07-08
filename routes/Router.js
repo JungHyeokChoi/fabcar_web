6047421cc59535a851afd76651e5a995ee7e9bc2
JungHyeokChoi <page1114@gmail.com>
2020-07-08 17:51:46 +0900

    Add change Owner

--- routes/Router.js
+++ routes/Router.js
@@ -10,8 +10,40 @@ router.route('/')
         res.render('index', {data : resultData})
     })
 
+router.route('/create')
+    .get((req, res) =>{
+        res.render('create')
+    })
+    .post(async (req, res, next) =>{
+        var key = req.body.key
+        var color = req.body.color
+        var make = req.body.make
+        var model = req.body.model
+        var owner = req.body.owner
+
+        await queryUtil.createCar(key, color, make, model, owner)
+        
+        await res.redirect('/')
+    })
+
+router.route('/changeOwner')
+    .get(async (req, res) => {
+        var result = await queryUtil.queryAllcars()
+        var resultData = await JSON.parse(result)
+
+        res.render('changeOwner', {data : resultData})
+    })
+    .post(async (req, res, next) =>{
+        var key = await req.body.key
+        var newOwner = await req.body.owner
+
+        await queryUtil.changeCarOwner(key, newOwner)
+
+        res.redirect('/')
+    })
+
 router.route('/search')
-    .get(async (req, res, next) => {
+    .get(async (req, res) => {
         var searchData = req.query.search
         var result = await queryUtil.queryCar(searchData)
         var resultData = await JSON.parse(result)
@@ -20,7 +52,7 @@ router.route('/search')
     })
 
 router.route('/detail/:carNum')
-    .get(async (req, res, next) => {
+    .get(async (req, res) => {
         var searchData = req.params.carNum
         var result = await queryUtil.queryCar(searchData)
         var resultData = await JSON.parse(result)
@@ -44,24 +76,8 @@ router.route('/detail/:carNum')
         })
     })
 
-router.route('/create')
-    .get((req,res,next) =>{
-        res.render('create')
-    })
-    .post(async (req, res, next) =>{
-        var key = req.body.key
-        var color = req.body.color
-        var make = req.body.make
-        var model = req.body.model
-        var owner = req.body.owner
-
-        await queryUtil.createCar(key, color, make, model, owner)
-        
-        await res.redirect('/')
-    })
-
 router.route('/save/:carNum')
-    .get(async (req, res, next) => {
+    .get(async (req, res) => {
         var searchData = req.params.carNum
         var result = await queryUtil.queryCar(searchData)
         var resultData = await JSON.parse(result)
